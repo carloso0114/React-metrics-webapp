864ffde50e77e75f59937f31e3d4dbfd25527451
carloso0114 <carlossospinag@gmail.com>
2021-09-15 16:18:12 -0500

    update UI

--- src/components/homePage/cards/cards.jsx
+++ src/components/homePage/cards/cards.jsx
@@ -1,14 +1,16 @@
+import { CardBlock, CardtTitle } from './styles';
+
 const cards = (props) => {
   const { data } = props;
   return (
-    <div className="col-6">
-      <h1>{data[0].toUpperCase()}</h1>
+    <CardBlock className="col-6">
+      <CardtTitle>{data[0].toUpperCase()}</CardtTitle>
       <p>
         Today cases confirmed:
         {' '}
         <span>{data[1].today_confirmed}</span>
       </p>
-    </div>
+    </CardBlock>
   );
 };
 
---
+++ src/components/homePage/cards/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CardBlock = styled.div`
+  color: white;
+  background-color: #4369B2;
+  border: solid 1px white;
+`;
+
+export const CardtTitle = styled.h1`
+  font-size: 1rem;
+`;
