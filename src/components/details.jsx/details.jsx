185f543d6c6a010fe98f6b7ab351260c22809c27
carloso0114 <carlossospinag@gmail.com>
2021-09-18 03:03:06 -0500

    update details page

--- src/components/details.jsx/details.jsx
+++ src/components/details.jsx/details.jsx
@@ -1,12 +1,44 @@
 import NavBar from '../navBar/navBar';
+import { Info, Title } from './styles';
 
 const details = (props) => {
   const { data } = props;
   return (
     <div>
       <NavBar />
-      <h1>{data[0]}</h1>
-      <p>{data[1].source}</p>
+      <div className="container-fluid text-center">
+        <Title>{data[0]}</Title>
+        <Info>
+          Total deaths:
+          {' '}
+          {data[1].today_deaths}
+        </Info>
+        <Info>
+          Today new deaths:
+          {' '}
+          {data[1].today_new_deaths}
+        </Info>
+        <Info>
+          Today new confirmed cases:
+          {' '}
+          {data[1].today_new_confirmed}
+        </Info>
+        <Info>
+          Total recovered:
+          {' '}
+          {data[1].today_recovered}
+        </Info>
+        <Info>
+          Today new recovered:
+          {' '}
+          {data[1].today_new_recovered}
+        </Info>
+        <Info>
+          Source of information:
+          {' '}
+          {data[1].source}
+        </Info>
+      </div>
     </div>
   );
 };
---
+++ src/components/details.jsx/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Info = styled.p`
+  font-size: 1.4rem;
+`;
+
+export const Title = styled.h1`
+  margin-top: 1rem;
+`;
