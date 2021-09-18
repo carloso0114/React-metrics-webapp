2aaba31968c1504f5340b2ccacace2083d0e209b
carloso0114 <carlossospinag@gmail.com>
2021-09-18 01:52:59 -0500

    clean reducer unnecesary function

--- src/redux/coronaData/coronaData.actions.js
+++ src/redux/coronaData/coronaData.actions.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 import dayjs from 'dayjs';
 import axios from 'axios';
-import { FETCH_DATA, GET_FILTERED } from './coronaData.types';
+import FETCH_DATA from './coronaData.types';
 
 const today = dayjs().subtract(2, 'day').format('YYYY-MM-DD');
 const covidAPI = `https://api.covid19tracking.narrativa.com/api/${today}`;
@@ -15,8 +15,3 @@ export const fetchData = () => async (dispatch) => {
   const res = await axios.get(covidAPI);
   dispatch(addData(res.data));
 };
-
-export const getFiltered = (data) => ({
-  type: GET_FILTERED,
-  payload: data,
-});
--- src/redux/coronaData/coronaData.types.js
+++ src/redux/coronaData/coronaData.types.js
@@ -1,2 +1,3 @@
-export const FETCH_DATA = 'FETCH_DATA';
-export const GET_FILTERED = 'FILTERED_DATA';
+const FETCH_DATA = 'FETCH_DATA';
+
+export default FETCH_DATA;
